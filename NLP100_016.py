aa01f04d309241f9857909654a3705c9468610db
neochiba <kawasaki2480b@icloud.com>
2017-07-19 22:01:15 +0900

    add new file

--- NLP100_016.py
+++ NLP100_016.py
@@ -1,21 +1,20 @@
 #coding UTF-8
+
 import math
+
 N = int(input())
 f = "hightemp.txt"
 a = math.ceil(24 / N)
 A = 0
+
 with open(f, 'r') as data_file:
     lines = data_file.readlines()
 
 for i in range(N):
     with open('col016' + str(i+1) + '.txt',  'w') as wf:
         word = lines[A:A+a]
-        #an = "".join(map(str, word))
-        #ans = an.rstrip()
-        #wf.write(ans)
         wf.writelines(word)
         A += a
 
-
 # UNIX操作
-# tail -n 10 hightemp.txt
+# split -l N(任意の数N) hightemp.txt col016
