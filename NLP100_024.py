8d99ceed81de2e5e577bf98865673b00c81c21a8
neo9996 <neo@mail>
2017-09-13 17:20:20 +0900

    add new file

---
+++ NLP100_024.py
@@ -0,0 +1,21 @@
+#coding UTF-8
+import json
+import gzip
+import re
+
+f = "jawiki-country.json.gz"
+
+def UK():
+    with gzip.open(f, 'r') as data_file:
+        for line in data_file:
+            obj = json.loads(line)
+            if obj['title'] == 'イギリス':
+                return obj['text']
+
+pattern = r'^.*?(?:ファイル:|File:)(.*?)(?:\|.*).*$'
+repatter = re.compile(pattern, re.MULTILINE)
+
+answer = repatter.findall(UK())
+
+for i in answer:
+    print(i)
